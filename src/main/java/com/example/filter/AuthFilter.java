9f375dcf4bab651fe22d87363b8a65f72f9919f5
Nikita Pyshchev <npyshchev@igatec.com>
2025-01-26 21:55:21 +0300

    fx

--- src/main/java/com/example/filter/AuthFilter.java
+++ src/main/java/com/example/filter/AuthFilter.java
@@ -7,16 +7,17 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @WebFilter(urlPatterns = "/user/*")
 public class AuthFilter implements Filter {
 
     @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+    public void doFilter(ServletRequest servletRequest, ServletResponse httpServletResponse, FilterChain filterChain) throws IOException, ServletException {
         Object attr = servletRequest.getAttribute("user");
         if (attr == null) {
-            servletRequest.getRequestDispatcher("/login.jsp");
+            ((HttpServletResponse) httpServletResponse).sendRedirect("/login.jsp");
         }
     }
 }
